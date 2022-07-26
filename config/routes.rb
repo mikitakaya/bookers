0062802c83ee436fa91b2751e1fc8d0393b3396d
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-07-26 11:11:45 +0000

    [Update]ルーティングの内容更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'books/new'
   get 'top' => 'homes#top'
-  resources :books
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
