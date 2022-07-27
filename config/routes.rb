cba5422e60a0964bed00c124b0276320e8c86884
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-07-27 08:29:11 +0000

    [Add]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'books/new'
   get 'top' => 'homes#top'
   post 'books' => 'books#create'
-  get '/bookers' => 'books#index'
+  get 'books' => 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
