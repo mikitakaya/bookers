c1a8dc497690127f902a5402ad686edcd128ed12
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-07-27 12:41:57 +0000

    [Add]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,31 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def index
    @books = Book.all
+   @book = Book.new
+  end
+
+  def create
+   # データを受け取り新規登録するためのインスタンス作成
+   book = Book.new(book_params)
+   # データをデータベースに保存するためのsaveメソッド実行
+   book.save
+   # 詳細画面へリダイレクト
+   redirect_to book_path(book.id)
   end
 
   def show
+  # レコード1件のみ取得するため、インスタンス変数名は単数形にする
+   @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+ private
+ # ストロングパラメータ
+ def book_params
+  params.require(:book).permit(:title, :body)
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/new'
+  get 'books/edit'
   get 'top' => 'homes#top'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
