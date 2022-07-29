bc4ba2ceb53240ec7ba7e819ccb90a05f0c761fb
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-07-29 10:06:57 +0000

    [Add]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,51 +1,51 @@
 class BooksController < ApplicationController
 
-  def index
+ def index
+  @books = Book.all
+  @book = Book.new
+ end
+
+ def create
+  # データを受け取り新規登録するためのインスタンス作成
+  @book = Book.new(book_params)
+  # モデルで返されたバリデーションの結果を、コントローラで検出する
+  if @book.save
+   # trueの場合、詳細画面（show）にリダイレクトする
+   redirect_to book_path(@book.id)
+  else
+   # falseの場合、投稿一覧画面（index）を再表示する
    @books = Book.all
-   @book = Book.new
+   render :index
   end
+ end
 
-  def create
-   # データを受け取り新規登録するためのインスタンス作成
-   @book = Book.new(book_params)
-   # モデルで返されたバリデーションの結果を、コントローラで検出する
-   if @book.save
-    # trueの場合、詳細画面（show）にリダイレクトする
-    redirect_to book_path(@book.id)
-   else
-    # falseの場合、投稿一覧画面（index）を再表示する
-    render :index
-   end
-  end
+ def show
+ # レコード1件のみ取得するため、インスタンス変数名は単数形にする
+  @book = Book.find(params[:id])
+ end
 
-  def show
-  # レコード1件のみ取得するため、インスタンス変数名は単数形にする
-   @book = Book.find(params[:id])
-  end
+ def edit
+  @book = Book.find(params[:id])
+ end
 
-  def edit
-   @book = Book.find(params[:id])
-  end
+ def update
+  book = Book.find(params[:id])
+  book.update(book_params)
+  redirect_to book_path(book.id)
+ end
 
-  def update
-   book = Book.find(params[:id])
-   book.update(book_params)
-   redirect_to book_path(book.id)
-  end
-
-  def destroy
-   # データ（レコード）を1件削除
-   book = Book.find(params[:id])
-   # データ（レコード）を削除
-   book.destroy
-   # index投稿一覧へリダイレクト
-   redirect_to '/books'
-  end
+ def destroy
+  # データ（レコード）を1件削除
+  book = Book.find(params[:id])
+  # データ（レコード）を削除
+  book.destroy
+  # index投稿一覧へリダイレクト
+  redirect_to '/books'
+ end
 
  private
  # ストロングパラメータ
  def book_params
   params.require(:book).permit(:title, :body)
  end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2022_07_26_095715) do
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ create_table "books", force: :cascade do |t|
+  t.string "title"
+  t.string "body"
+  t.datetime "created_at", precision: 6, null: false
+  t.datetime "updated_at", precision: 6, null: false
+ end
 
 end
